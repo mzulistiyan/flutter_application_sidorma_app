0d24a904a2f2a2559cc7b1b64c8b7ffa05ddb7a9
izulistiyan@gmail.com <izulistiyan@gmail.com>
2023-06-04 19:54:23 +0700

    update 4 Juni

--- lib/core/constant/network.dart
+++ lib/core/constant/network.dart
@@ -1,5 +1,5 @@
 class NETWORK {
-  static const baseUrlDevelopment = 'https://e781-103-156-164-24.ngrok-free.app';
+  static const baseUrlDevelopment = 'https://79fb-103-156-164-13.ngrok-free.app';
   // static const baseUrlProduction = '';
   static const oneSignalAppId = 'dbafa003-d3a0-42a5-8a8f-f97052043ea2';
 }
--- lib/feature/home/service/home_services.dart
+++ lib/feature/home/service/home_services.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_application_sidorma/core/api.dart';
+import 'package:flutter_application_sidorma/feature/home/models/response_get_report_today.dart';
 
 class HomeServices {
   Future<Response<dynamic>?> absensi() async {
@@ -36,6 +40,20 @@ class HomeServices {
       return response;
     } on DioError catch (e) {
       return e.response ?? Response(requestOptions: RequestOptions(path: ''));
+    }
+  }
+
+  Future<DataAbsensiToday> getReportToday() async {
+    final response = await Api().get(
+      path: 'api/report?today=true',
+    );
+    if (response.statusCode == 200) {
+      debugPrint("testing 200");
+      var data = jsonDecode(response.data);
+      var parsed = data['data'] as Map<String, dynamic>;
+      return DataAbsensiToday.fromJson(parsed);
+    } else {
+      throw Exception('Failed to load mahasiswa');
     }
   }
 }
