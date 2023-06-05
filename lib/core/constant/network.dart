a4424074135d11ea32957fdd284ff397a8f0629e
ammarff551@gmail.com <ammarff551@gmail.com>
2023-06-05 19:12:07 +0700

    fix-conflict

--- lib/core/constant/network.dart
+++ lib/core/constant/network.dart
@@ -1,10 +1,5 @@
 class NETWORK {
-<<<<<<< HEAD
   static const baseUrlDevelopment =
       'https://506d-103-156-164-13.ngrok-free.app';
-  // static const baseUrlProduction = '';
-=======
-  static const baseUrlDevelopment = 'https://506d-103-156-164-13.ngrok-free.app';
->>>>>>> d1eb01e0af1a3333eca98ca566a381351252f905
   static const oneSignalAppId = 'dbafa003-d3a0-42a5-8a8f-f97052043ea2';
 }
--- lib/feature/home/screen/report_screen.dart
+++ lib/feature/home/screen/report_screen.dart
@@ -1,30 +1,5 @@
-<<<<<<< HEAD
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:flutter_application_sidorma/core/api.dart';
-import 'package:flutter_application_sidorma/feature/home/models/response_get_report_today.dart';
-
-Future<List<DataAbsensiToday>> getReportToday() async {
-  final response = await Api().get(
-    path: 'api/report?today=true',
-  );
-  if (response.statusCode == 200) {
-    debugPrint("testing 200");
-    var data = jsonDecode(response.data);
-    debugPrint("Data Testing : $data");
-    var parsed = data['data'].cast<Map<String, dynamic>>();
-    return parsed
-        .map<DataAbsensiToday>((json) => DataAbsensiToday.fromJson(json))
-        .toList();
-  } else {
-    throw Exception('Failed to load mahasiswa');
-  }
-}
-=======
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
->>>>>>> d1eb01e0af1a3333eca98ca566a381351252f905
 
 class ReportScreen extends StatefulWidget {
   const ReportScreen({super.key});
@@ -34,89 +9,8 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
-<<<<<<< HEAD
-  late Future<List<DataAbsensiToday>> futureReport;
-
-  @override
-  void initState() {
-    super.initState();
-    futureReport = getReportToday();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<List<DataAbsensiToday>>(
-        future: futureReport,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List<DataAbsensiToday>? data = snapshot.data;
-            return ListView.builder(
-              itemCount: data!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  color: Colors.white,
-                  child: ListTile(
-                    title: Text(data[index].nimMahasiswa),
-                  ),
-                );
-              },
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-        // padding: const EdgeInsets.all(24),
-        // children: [
-        //   Container(
-        //       padding: const EdgeInsets.all(10),
-        //       decoration: BoxDecoration(
-        //         border: Border.all(),
-        //         borderRadius: BorderRadius.circular(8),
-        //       ),
-        //       child: Row(
-        //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //         children: [
-        //           Column(
-        //             children: [
-        //               Row(
-        //                 children: [
-        //                   Image.asset(
-        //                     'assets/images/icons/ic_card_report_absen.png',
-        //                     width: 32,
-        //                   ),
-        //                   const SizedBox(
-        //                     width: 20,
-        //                   ),
-        //                   Column(
-        //                     crossAxisAlignment: CrossAxisAlignment.start,
-        //                     children: const [Text("Minggu"), Text('data')],
-        //                   ),
-        //                 ],
-        //               ),
-        //             ],
-        //           ),
-        //           Container(
-        //             width: 50,
-        //             height: 25,
-        //             decoration: BoxDecoration(
-        //                 border: Border.all(color: Colors.grey),
-        //                 color: Colors.green),
-        //             child: const Center(
-        //               child: Text('pp'),
-        //             ),
-        //           ),
-        //         ],
-        //       )),
-        // ],
-      ),
-    );
-=======
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
->>>>>>> d1eb01e0af1a3333eca98ca566a381351252f905
   }
 }
